698326b605635db9c9e1fbfa83be8e4168d85cc9
Shona-S <727823tuam047@skct.edu.in>
2024-12-07 15:54:27 +0530

    finished look

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,7 +7,6 @@ const LandingPage = () => {
     <div className="landing-page">
       <div className="background-image">
         <h1>Plant Shop</h1>
-        <p>We bring you the best houseplants to brighten your home!</p>
         <Link to="/product-listing" className="get-started-button">Get Started</Link>
       </div>
     </div>
